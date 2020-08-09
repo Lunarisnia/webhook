4a2c473910e7b472e34775a88ca7cbe358b4a6e1
loona <r.arswendo.r@gmail.com>
2020-08-09 21:08:54 +0700

    [FIX] BUGGS

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ router.post("/github", async (req, res) => {
   const resp = await axios.post(
     `https://api.callmebot.com/whatsapp.php?phone=${process.env.RIO_PHONE}&text=itwork&apikey=${process.env.RIO_KEY}`
   );
-  res.send("oke");
+  res.send("okeeeeeeeeeee");
 });
 
 module.exports = router;
