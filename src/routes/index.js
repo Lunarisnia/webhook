3e848982bcd410496bdd2e5d47b378ff00a2b9ad
loona <r.arswendo.r@gmail.com>
2020-08-09 21:35:22 +0700

    [FIX] Template spacing

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,16 @@ const moment = require("moment");
 
 router.post("/github", async (req, res) => {
   const body = req.body;
-  const message = `
-  *${moment(body.head_commit.timestamp).format("MMMM Do YYYY, h:mm:ss a")}*
-  *${body.pusher.name}* Pushed *${body.head_commit.message}* at *${
-    body.ref.split("/")[body.ref.split("/").length - 1]
-  }*
+  const message = 
+`
+*${moment(body.head_commit.timestamp).format("MMMM Do YYYY, h:mm:ss a")}*
+*${body.pusher.name}* Pushed *${body.head_commit.message}* at *${
+body.ref.split("/")[body.ref.split("/").length - 1]
+}*
   
-  Repo: *${body.repository.name}*
-  Branch: *${body.ref.split("/")[body.ref.split("/").length - 1]}*
-  URL: ${body.repository.url}`;
+Repo: *${body.repository.name}*
+Branch: *${body.ref.split("/")[body.ref.split("/").length - 1]}*
+URL: ${body.repository.url}`;
   const resp = await Promise.all([
     axios.post(
       `${process.env.WA_API}?phone=${process.env.RIO_PHONE}&text=${message}&apikey=${process.env.RIO_KEY}`
