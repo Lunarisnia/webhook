e914fb820b12d4485e51b5fe472e687b5e0cf501
loona <r.arswendo.r@gmail.com>
2020-08-09 21:32:31 +0700

    [FIX] Messages template

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,13 @@ router.post("/github", async (req, res) => {
   const body = req.body;
   const message = `
   *${moment(body.head_commit.timestamp).format("MMMM Do YYYY, h:mm:ss a")}*
-  *${body.pusher.name}* Pushed ${body.head_commit.message} at *${
+  *${body.pusher.name}* Pushed *${body.head_commit.message}* at *${
     body.ref.split("/")[body.ref.split("/").length - 1]
   }*
   
   Repo: *${body.repository.name}*
-  Branch: *${body.ref.split("/")[body.ref.split("/").length - 1]}*`;
+  Branch: *${body.ref.split("/")[body.ref.split("/").length - 1]}*
+  URL: ${body.repository.url}`;
   const resp = await Promise.all([
     axios.post(
       `${process.env.WA_API}?phone=${process.env.RIO_PHONE}&text=${message}&apikey=${process.env.RIO_KEY}`
