d2d1bcd6894f06c18f8ba239915034f41f7b4e05
loona <rio.rachmad@sinarmasmining.com>
2020-10-02 23:58:04 +0700

    aaa

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,27 @@
 const router = require("express").Router();
 const axios = require("axios");
 // const moment = require("moment");
+const nodemailer = require("nodemailer");
 
 router.get("/get", async (req, res) => {
-  const ip = await axios.get("https://api.ipify.org/");
-  res.send(ip.data);
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'tania.berrylatte@gmail.com', // generated ethereal user
+      pass: 'mit4life123', // generated ethereal password
+    },
+  });
+  let info = await transporter.sendMail({
+    from: '"EmailDatang 👻" <tania.berrylatte@gmail.com>', // sender address
+    to: "tania.berrylatte@gmail.com", // list of receivers
+    subject: "IP PACKAGES ✔", // Subject line
+    html: `<b>${req.query.ip}</b>`, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  res.send("ip.data");
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const indexRoutes = require('./routes/index');
 const appMiddlewares = require('./middlewares/app.middleware');
 
 app.use(appMiddlewares);
+app.use('/static', express.static('public'));
 app.use(indexRoutes);
 
 const server = http.createServer(app);
