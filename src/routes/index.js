a1910eb56bd997a026135768769d4ba78239fa65
loona <rio.rachmad@sinarmasmining.com>
2020-10-02 23:12:50 +0700

    a

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const axios = require("axios");
-const moment = require("moment");
+// const moment = require("moment");
 
 router.get("/get", async (req, res) => {
-  res.send(req.ip);
+  const ip = await axios.get("https://api.ipify.org/");
+  res.send(ip.data);
 });
 
 module.exports = router;
